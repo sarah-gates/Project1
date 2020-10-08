6d4f394e234bce5d0f25b09965ede23b97b4acae
sarah-gates <70338153+sarah-gates@users.noreply.github.com>
2020-10-08 01:34:38 -0500

    Update DateTimeOne.java

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -9,18 +9,18 @@ public class DateTimeOne
 	public void dateTimeNow() 
 	{
 		//Date/time object for date/time
-		DateTimeFormatter formattedDateTime = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
-		LocalDateTime showDateTime = LocalDateTime.now();
-		String formattedDateTimeFinal = showDateTime.format(formattedDateTime);
-	    System.out.println("Current Date/Time: " + formattedDateTimeFinal);	    
+		DateTimeFormatter formattedDateTime = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a"); //creates format for date
+		LocalDateTime showDateTime = LocalDateTime.now(); //gets current date
+		String formattedDateTimeFinal = showDateTime.format(formattedDateTime); //formats current date into format
+	    System.out.println("Current Date/Time: " + formattedDateTimeFinal);	    //prints date
 	}
 	public int getValueOfSecond() {
-		LocalDateTime date = LocalDateTime.now();
-		int second = date.getSecond();
+		LocalDateTime date = LocalDateTime.now(); //gets date
+		int second = date.getSecond(); //gets second
 		return second;
 	}
 	public void sleepForThreeSec() throws InterruptedException {
-		TimeUnit.SECONDS.sleep(3);
+		TimeUnit.SECONDS.sleep(3); //makes it sleep for 3 seconds
 		
 	}
 }
